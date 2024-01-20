1fabb5ee9c219849c8b317a922f7d4a42f133b47
AlexanderUridin <a89154850307@gmail.com>
2024-01-20 19:17:10 +0300

    add flake8

--- gendiff/help.py
+++ gendiff/help.py
@@ -4,7 +4,7 @@ import argparse
 def help():
     parser = argparse.ArgumentParser(
         description='Compares two configuration files and shows a difference.')
-    
+
     parser.add_argument('first_file')
     parser.add_argument('second_file')
     args = parser.parse_args()
