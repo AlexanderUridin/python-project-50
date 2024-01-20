9339aa8e8cf3ca0b2e06f406fd53ea9d8e9673ae
AlexanderUridin <a89154850307@gmail.com>
2024-01-20 19:29:06 +0300

    add optional arg in help.py

--- gendiff/help.py
+++ gendiff/help.py
@@ -7,5 +7,7 @@ def help():
 
     parser.add_argument('first_file')
     parser.add_argument('second_file')
+    parser.add_argument('-f', '--format',
+                        help ='set format of output')
     args = parser.parse_args()
     return args
