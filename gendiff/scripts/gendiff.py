e2aed052d7b10531dc64e0e78383843d9549aa38
AlexanderUridin <a89154850307@gmail.com>
2024-01-20 20:59:13 +0300

    fix lint

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -10,7 +10,7 @@ def main():
     parser.add_argument('first_file')
     parser.add_argument('second_file')
     parser.add_argument('-f', '--format',
-                        help ='set format of output')
+                        help='set format of output')
     args = parser.parse_args()
 
     diff = generate_diff(args.first_file, args.second_file)
