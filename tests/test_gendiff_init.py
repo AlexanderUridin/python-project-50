572792bade50b4570a2ec9953830856031d39928
AlexanderUridin <a89154850307@gmail.com>
2024-01-21 14:59:27 +0300

    add yaml diff test

--- tests/test_gendiff_init.py
+++ tests/test_gendiff_init.py
@@ -1,9 +1,19 @@
 from gendiff.__init__ import generate_diff
 
 
-def test_generate_diff():
+def test_generate_diff_json():
     file_path1 = './tests/fixtures/file1.json'
     file_path2 = './tests/fixtures/file2.json'
     result = generate_diff(file_path1, file_path2)
-    correct = open('./tests/fixtures/result_json.txt')
+    correct = open('./tests/fixtures/result.txt')
+
+    assert result == correct.read()
+
+
+def test_generate_diff_yaml():
+    file_path1 = './tests/fixtures/file1.yml'
+    file_path2 = './tests/fixtures/file2.yml'
+    result = generate_diff(file_path1, file_path2)
+    correct = open('./tests/fixtures/result.txt')
+    
     assert result == correct.read()
